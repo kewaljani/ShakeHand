670cf1168915a6b03daadba35fc3a570258d2574
Mudra Koradia <50293122+mudra523@users.noreply.github.com>
2021-07-28 23:13:08 +0530

    changes

--- src/Institution/Profile.jsx
+++ src/Institution/Profile.jsx
@@ -3,7 +3,11 @@ import ILayout from './../ILayout/Index'
 
 function Profile(props) {
   return (
-    <ILayout><div className="bg_app_green" style={{ width: "100vw", height: '200px' }}>Profile</div></ILayout>
+    <ILayout>
+      <div className="bg_app_green" style={{ width: "100vw", height: '200px' }}>
+        Profile
+      </div>
+    </ILayout>
   );
 }
 
