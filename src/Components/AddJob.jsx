f1c96c7feb8b83197cdbd595ee00daaff1031667
kewal jani <kewal.jani@gmail.com>
2021-08-27 13:47:39 -0400

    Edited button

--- src/Components/AddJob.jsx
+++ src/Components/AddJob.jsx
@@ -114,9 +114,17 @@ function AddJob(props) {
                 Submit
               </Button>
             </Form.Item>
+            <Form.Item
+              style={{ display: "flex", justifyContent: "end", float: "right" }}
+            >
+              <Button type="primary" htmlType="submit" className="green_button">
+                Submit
+              </Button>
+            </Form.Item>
           </Form>
         </Card>
       </Col>
+      
     </Row>
   );
 }
